f01e0603a9f4adf1562d5c02ff7ca47b9492f30a
Andy <andreadiazregules@gmail.com>
2024-12-06 02:33:29 -0600

    hice cambio

--- test/gestorproyectos/modelo/dao/AlumnoDAOTest.java
+++ test/gestorproyectos/modelo/dao/AlumnoDAOTest.java
@@ -45,17 +45,12 @@ public class AlumnoDAOTest {
 
     @Test(expected = SQLException.class)
     public void testRegistrarAlumnoExcepcionSQL() throws SQLException {
-        alumno = new Alumno(
-                null, // ID se genera automáticamente
-                "Tomas",
-                "Gutierrez",
-                "S22013679", // Usar una matrícula que ya exista en la base de datos
-                "123456789",
-                "tomas@correo.com", // Usar un correo que ya exista
-                9.5f,
-                "inscrito"
+        alumno = new Alumno(null, "Tomas", "Gutierrez",
+                "S22013679", "+52 1234567890", "tomas@correo.com",
+                9.5f, "inscrito"
         );
 
-        AlumnoDAO.registrarAlumno(alumno); // Debería lanzar SQLException
+        AlumnoDAO.registrarAlumno(alumno);
     }
 }
+
