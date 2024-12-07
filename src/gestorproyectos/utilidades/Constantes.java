f6e433f934903090e48cf3d34e6c61b90a3603bf
Ronnie <veromota.corn@gmail.com>
2024-12-06 21:08:55 -0600

    hice cambios

--- src/gestorproyectos/utilidades/Constantes.java
+++ src/gestorproyectos/utilidades/Constantes.java
@@ -2,7 +2,7 @@ package gestorproyectos.utilidades;
 
 public class Constantes {
 
-    public static final String USER_BD = "root";
-    public static final String PASSWORD_BD = "121404";
+    public static final String USER_BD = "AdminBD";
+    public static final String PASSWORD_BD = "Password1.";
 
 }
