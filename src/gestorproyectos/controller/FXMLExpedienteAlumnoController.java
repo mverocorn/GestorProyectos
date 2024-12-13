88a09c8149674e5c1730e990b401eb1ce224ddf1
jonathan <jonathan@LAPTOP-FHKTQCJA>
2024-12-13 04:49:10 -0600

    Funcionalidad a expediente alumno junto con modificacion de botones

--- src/gestorproyectos/controller/FXMLExpedienteAlumnoController.java
+++ src/gestorproyectos/controller/FXMLExpedienteAlumnoController.java
@@ -5,7 +5,14 @@ import gestorproyectos.modelo.dao.ExpedienteDAO;
 import gestorproyectos.modelo.dao.InscripcionEEDAO;
 import gestorproyectos.modelo.dao.ReporteDAO;
 import gestorproyectos.modelo.pojo.Documento;
+import gestorproyectos.modelo.pojo.EE;
+import gestorproyectos.modelo.pojo.Empresa;
 import gestorproyectos.modelo.pojo.Expediente;
+import gestorproyectos.modelo.pojo.InscripcionEE;
+import gestorproyectos.modelo.pojo.ProyectoPP;
+import gestorproyectos.modelo.pojo.ProyectoSS;
+import gestorproyectos.modelo.pojo.Reporte;
+import gestorproyectos.modelo.pojo.Responsable;
 import gestorproyectos.utilidades.MisUtilidades;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -13,8 +20,12 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -22,6 +33,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.DirectoryChooser;
 
 /**
@@ -48,28 +60,64 @@ public class FXMLExpedienteAlumnoController implements Initializable {
 	@FXML
 	private TableView<Documento> tblDocumentos;
 	@FXML
-	private TableColumn<?, ?> colNombreDocumento;
+	private TableColumn colNombreDocumento;
 	@FXML
-	private TableColumn<?, ?> colValidar;
+	private TableColumn colValidar;
 	@FXML
-	private TableView<?> tblReportes;
+	private TableView<Reporte> tblReportes;
 	@FXML
-	private TableColumn<?, ?> colNombreReporte;
+	private TableColumn colNombreReporte;
 	@FXML
-	private TableColumn<?, ?> colHoras;
+	private TableColumn colHoras;
         private int idInscripcionEE;
+        private int idExpediente;
+        private Empresa empresa;
+        private Responsable responsable;
+        private InscripcionEE inscripcionEE;
+        private Expediente expediente;
+        private ObservableList<Documento> documentos;
+        private ObservableList<Reporte> reportes;
+        private ProyectoSS proyectoSS;
+        private ProyectoPP proyectoPP;
+        private boolean tipoProyectoPP;
+        private EE ee;
 
 	/**
 	 * Initializes the controller class.
 	 */
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		// TODO
-	}	
+            llenarTablaDocumentos();
+            if(tipoProyectoPP){
+            llenarInformacionPP();
+            }else{
+            llenarInformacionSS(); 
+            }
+	}
         
-        public void inicializarValores (int idInscripcionEE){
-            this.idInscripcionEE=idInscripcionEE;
+        public void inicializarValores (Empresa empresa, Responsable responsable,EE ee, InscripcionEE inscripcionEE, Expediente expediente, ProyectoPP proyectoPP){
+            this.empresa=empresa;
+            this.responsable=responsable;
+            this.ee=ee;
+            this.inscripcionEE=inscripcionEE;
+            this.expediente=expediente;
+            this.idInscripcionEE=inscripcionEE.getIdInscripcionEE();
+            this.idExpediente=expediente.getIdExpediente();
+            this.proyectoPP=proyectoPP;
+            tipoProyectoPP=true;
         }
+        
+        public void inicializarValores (Empresa empresa, Responsable responsable,EE ee, InscripcionEE inscripcionEE, Expediente expediente, ProyectoSS proyectoSS){
+            this.empresa=empresa;
+            this.responsable=responsable;
+            this.ee=ee;
+            this.inscripcionEE=inscripcionEE;
+            this.expediente=expediente;
+            this.idInscripcionEE=inscripcionEE.getIdInscripcionEE();
+            this.idExpediente=expediente.getIdExpediente();
+            this.proyectoSS=proyectoSS;
+            tipoProyectoPP=false;
+        }        
 
 	@FXML
 	private void clickDarDeBaja(ActionEvent event) {
@@ -114,6 +162,7 @@ public class FXMLExpedienteAlumnoController implements Initializable {
         }
     }        
 	
+    @FXML
     private void clicValidar() {
         boolean confirmarValidar = MisUtilidades.crearAlertaConfirmacion("Confirmacion", "Seguro que desea validar el documento, esta accion no se puede deshacer");
         if(confirmarValidar){
@@ -173,5 +222,83 @@ public class FXMLExpedienteAlumnoController implements Initializable {
             MisUtilidades.crearAlertaSimple(Alert.AlertType.WARNING,"Error al cargar el archivo", "No se encontro el archivo que deseas descargar");
         }
     }    
+
+    @FXML
+    private void clicDescargar(ActionEvent event) {
+        clicDescargarDocumento();
+    }
+
+    private void llenarTablaDocumentos() {
+        configurarTablaDocumentos();
+        cargarInformacionTablaDocumentos();
+    }
     
+     private void configurarTablaDocumentos() {
+        colNombreDocumento.setCellValueFactory(new PropertyValueFactory("nombreDocumento"));
+        colValidar.setCellValueFactory(new PropertyValueFactory("validado"));
+    }
+
+    private void cargarInformacionTablaDocumentos() {
+        documentos = FXCollections.observableArrayList();
+        try{
+        List<Documento> documentosBD = DocumentoDAO.obtenerDocumentos(idExpediente);
+        if(!documentosBD.isEmpty()){
+            documentos.addAll(documentosBD);
+            tblDocumentos.setItems(documentos);
+        }else{
+            MisUtilidades.crearAlertaSimple(Alert.AlertType.ERROR,"Error", "Lo sentimos no se puede cargar en este "
+                    + "momento la informacion de los clientes");
+        }
+        }catch(SQLException e){
+            MisUtilidades.crearAlertaSimple(Alert.AlertType.ERROR,"Error", "Lo sentimos, no se puede cargar la informacion en este momento ");
+        }
+    }   
+    
+    private void llenarTablaReportes() {
+        configurarTablaReportes();
+        cargarInformacionTablaReportes();
+    }
+    
+     private void configurarTablaReportes() {
+        colNombreReporte.setCellValueFactory(new PropertyValueFactory("nombreReporte"));
+        colHoras.setCellValueFactory(new PropertyValueFactory("horasReportadas"));
+    }
+
+    private void cargarInformacionTablaReportes() {
+        reportes = FXCollections.observableArrayList();
+        try{
+        HashMap<String,Object> respuesta = ReporteDAO.obtenerReportes(idExpediente);
+        boolean isError = (boolean) respuesta.get("error");
+        if(!isError){
+            ArrayList<Reporte> reportesBD = (ArrayList<Reporte>) respuesta.get("reportes");
+            reportes.addAll(reportesBD);
+            tblReportes.setItems(reportes);
+        }else{
+            MisUtilidades.crearAlertaSimple(Alert.AlertType.ERROR,"Error", "Lo sentimos no se puede cargar en este "
+                    + "momento la informacion de los clientes");
+        }
+        }catch(SQLException e){
+            MisUtilidades.crearAlertaSimple(Alert.AlertType.ERROR,"Error", "Lo sentimos, no se puede cargar la informacion en este momento ");
+        }
+    }     
+
+    private void llenarInformacionPP() {
+        lblResultadoProyectp.setText(proyectoPP.getNombreProyecto());
+        lblResultadoEmpresa.setText(empresa.getNombreEmpresa());
+        lblResultadoResponsable.setText(responsable.getNombreResponsable() + responsable.getApellidoResponsable());
+        lblResultadoPeriodo.setText(ee.getPeriodo());
+        lblResultadoFecha.setText(proyectoPP.getFechaProyecto());
+        lblResultadoDescripcion.setText(proyectoPP.getDescripcionProyecto());
+        lblResultadoObjetivo.setText(proyectoPP.getObjetivoProyecto());
+    }
+    
+    private void llenarInformacionSS() {
+        lblResultadoProyectp.setText(proyectoSS.getNombreProyecto());
+        lblResultadoEmpresa.setText(empresa.getNombreEmpresa());
+        lblResultadoResponsable.setText(responsable.getNombreResponsable() + responsable.getApellidoResponsable());
+        lblResultadoPeriodo.setText(ee.getPeriodo());
+        lblResultadoFecha.setText(proyectoSS.getFechaProyecto());
+        lblResultadoDescripcion.setText(proyectoSS.getDescripcionProyecto());
+        lblResultadoObjetivo.setText(proyectoSS.getObjetivoProyecto());
+    }    
 }
